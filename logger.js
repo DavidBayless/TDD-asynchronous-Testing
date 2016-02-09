c69b724be179332ed51972c2736b3ee28a2dcbf4
David Bayless <David.C.Bayless15@gmail.com>
2016-02-09 11:47:18 -0700

    passed first test creating file

--- logger.js
+++ logger.js
@@ -1,5 +1,12 @@
+var fs = require('fs');
+
 function lumberyard() {
 
+  this.init = function(fileName, callback) {
+    fs.writeFile(fileName, 'Hello World', function(err) {
+      callback();
+    });
+  };
 }
 
 module.exports = lumberyard;
--- test/logger.test.js
+++ test/logger.test.js
@@ -1,17 +1,25 @@
-var fileTest = require('../logger.js');
+var Logger = require('../logger.js');
 var expect = require('chai').expect;
+var fs = require('fs');
 
 describe('The great lumberjack', function() {
 
-  it('Should create a new file', function() {
-
+  it('Should create a new file', function(done) {
+    var log = new Logger();
+    var fileName = 'logger.log';
+    log.init('logger.log', function(err) {
+      fs.stat(fileName, function(err, data) {
+        if (err || !data.isFile()) {
+          done(err);
+        } else {
+          expect(data.isFile()).to.equal(true);
+          done();
+        }
+      });
+    });
   });
 
-  it('Should prepend LOG to argument', function() {
+  it('Should prepend LOG to argument');
 
-  });
-
-  it('Should prepend INFO to argument and save file', function() {
-
-  });
+  it('Should prepend INFO to argument and save file');
 });
